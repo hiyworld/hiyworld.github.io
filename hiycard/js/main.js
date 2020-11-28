be7142d5ffbc58f7bfe9e361f6ef53dc484b7e26
hiyworld <75175856+hiyworld@users.noreply.github.com>
2020-11-29 01:18:41 +0530

    correction

--- hiycard/js/main.js
+++ hiycard/js/main.js
@@ -3,6 +3,6 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('../sw.js');
+             .register('https://hiyworld.github.io/hiycard/sw.js');
   }
 }
